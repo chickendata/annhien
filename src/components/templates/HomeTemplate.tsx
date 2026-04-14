39df4899bb0635b56cb4122eff79d8e3739b018c
Nguyen Viet Phuong <nvp.1312.2001@gmail.com>
2026-04-14 22:11:54 +0700

    Remove brand to home page

--- src/components/templates/HomeTemplate.tsx
+++ src/components/templates/HomeTemplate.tsx
@@ -3,7 +3,6 @@ import CollectionGrid from "@/components/organisms/CollectionGrid";
 import NewArrivalsCarousel from "@/components/organisms/NewArrivalsCarousel";
 import StylistProgram from "@/components/organisms/StylistProgram";
 import BlogLatest from "@/components/organisms/BlogLatest";
-import BrandsRow from "@/components/organisms/BrandsRow";
 import { getNewArrivals } from "@/data/products";
 
 export default function HomeTemplate({ locale }: { locale: "vi" | "en" }) {
@@ -15,7 +14,6 @@ export default function HomeTemplate({ locale }: { locale: "vi" | "en" }) {
       <NewArrivalsCarousel products={newArrivals} locale={locale} />
       <StylistProgram />
       <BlogLatest locale={locale} />
-      <BrandsRow />
     </>
   );
 }
